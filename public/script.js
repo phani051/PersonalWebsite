d65ddc4a9ee9b7633497d459cdb762a689bf3dee
Phani051 <phani051@gmail.com>
2022-09-08 21:04:50 +0530

    Added second page and navbar

--- public/script.js
+++ public/script.js
@@ -1,18 +1,10 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
     }
   }
-  
-  window.addEventListener("scroll", reveal);
-  
+);
