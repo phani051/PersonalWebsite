32ba5a2ce70f2018ec07ddc98756afe0b65ee786
phani051 <phani051@gmail.com>
2022-09-25 20:16:16 +0530

    Modified package.json

---
+++ build/precache-manifest.166b53905f571102b93788089686962d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f05365c71ce3230051e6",
+    "url": "/PersonalWebsite/static/js/main.f05365c7.chunk.js"
+  },
+  {
+    "revision": "35aaa751ebde4c94d3cf",
+    "url": "/PersonalWebsite/static/js/1.35aaa751.chunk.js"
+  },
+  {
+    "revision": "2997cd6cd44eaed4d4af",
+    "url": "/PersonalWebsite/static/js/runtime~main.2997cd6c.js"
+  },
+  {
+    "revision": "481339e81919dcf1638122651d77c231",
+    "url": "/PersonalWebsite/index.html"
+  }
+];
